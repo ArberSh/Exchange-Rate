38b53c5e801071bb94cbeb1389671711c7154ba4
ArberSh <arbershaska2@gmail.com>
2023-09-05 23:46:46 +0200

    Trying to fix errors

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 console.log("works")
 const apiKey = "11be19509942028a46e7605d"
+
 const numb = document.querySelector(".PutNumber")
 const Result = document.querySelector(".Result")
 const test = document.querySelector(".test")
-async function getRate(){
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${test.value}/`)
+
+let DataRate = []
+async function getRate(currency){
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency}/`)
     const data = await response.json()
     console.log(data)
-    const exchangerate = data.conversion_rates
-    console.log(exchangerate.EUR)
-    numb.value = numb.value * exchangerate.EUR
-    Result.innerHTML = `<h1>${numb.value}</h1>`
-    numb.value = ''
+    DataRate = data.conversion_rates
+    console.log(DataRate.EUR)
+}
+function getOption() {const fromDropdown = document.querySelector('#Currency');
+    fromDropdown.innerHTML = ''; // Clear existing options
+
+    for (const currency in DataRate) {
+        const option = document.createElement('option');
+        option.value = currency;
+        option.text = currency;
+        fromDropdown.appendChild(option);
+    }
+    const selectedCurrency = document.querySelector('#Currency').value;
+    getRate(selectedCurrency)
+    console.log(selectedCurrency);
 }
 
+function populateFromDropdown() {
+    
+}
+
+function Results(){
+    numb.value = numb.value * DataRate.EUR
+    Result.innerHTML = `<h1>${numb.value}</h1>`
+    numb.value = ''
+    console.log("works")
+}
+console.log(DataRate.EUR)
